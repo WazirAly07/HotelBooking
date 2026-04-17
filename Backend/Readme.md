aac1f1cf38cc608ec175941400bfa493d4b03107
Muhammad Ali <161317819+WazirAly07@users.noreply.github.com>
2026-04-17 22:20:51 +0500

    Delete Backend directory

--- Backend/Readme.md
+++
@@ -1,6 +0,0 @@
-Volunteer Facilitation System 
-
-Member:
-Muhammad Ali
-Zill-e-Abbas Haider 
-Syed Hassan Shah.
--- Backend/src/routes/inquiry.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createInquiry, getAllInquiries } from "../controllers/inquiryController.js";
-
-const router = express.Router();
-
-router.post("/", createInquiry);
-router.get("/", getAllInquiries);
-
-export default router;
